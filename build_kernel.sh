0f608f9edae176d6cc6df4cabd01d94f8f88ef46
ByShowSkinger49 <marina1488gsgsgs@gmail.com>
2026-09-08 23:03:02 +0300

    build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-export PLATFORM_VERSION=11
+export PLATFORM_VERSION=15
 export ANDROID_MAJOR_VERSION=r 
 export ARCH=arm64
-export SEC_BUILD_CONF_VENDOR_BUILD_OS=13
+export SEC_BUILD_CONF_VENDOR_BUILD_OS=15
 make exynos9830-x1slte_defconfig
 make
+#Skinger49 patch
